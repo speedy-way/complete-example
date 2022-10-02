2062e99b59d092b8dd5439c03cdf74bdd97aa817
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-10-02 15:54:16 -0300

    add zone and delegation set

--- base/main.tf
+++ base/main.tf
@@ -1,4 +1,9 @@
 module "base" {
   source       = "git::https://github.com/speedy-way/terraform-aws-speedy-base.git?ref=main"
   organization = "speedy-way"
+
+  create-shared-albs = true
+  create-zone        = true
+  zone               = "speedy-way.xyz"
+  delegation-set-id  = "N0857411397BX2J2BMTV0"
 }
